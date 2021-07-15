0c28820c8fb0942edfa289a0197f6a356f975d77
HB <54346657+yourstrulyhb@users.noreply.github.com>
2021-07-16 06:33:39 +0800

    Add files via upload

---
+++ empathy-countdown/assets/app.js
@@ -0,0 +1,26 @@
+const endDatetime = new Date(`July 26 2021 17:00:00`);
+console.log(endDatetime);
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+// Update Countdown
+function updateCountdownTime() {
+
+   const currentTime = new Date();
+   const timeDiff = endDatetime - currentTime;     // amount of time (seconds) 'til end date
+
+   const daysLeft = Math.floor(timeDiff / 1000 / 60 / 60 / 24);
+   const hrsLeft = Math.floor(timeDiff / 1000 / 60 / 60) % 24;
+   const minsLeft = Math.floor(timeDiff / 1000 / 60) % 60;
+   const secondsLeft = Math.floor(timeDiff / 1000) % 60;
+
+   // Update display
+   days.innerHTML = daysLeft;
+   hours.innerHTML = hrsLeft < 10? '0' + hrsLeft: hrsLeft;
+   minutes.innerHTML = minsLeft < 10 ? '0' + minsLeft : minsLeft;
+   seconds.innerHTML = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+}
+
+setInterval(updateCountdownTime, 1000);
